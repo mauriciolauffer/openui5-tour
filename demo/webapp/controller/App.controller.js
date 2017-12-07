2c00938e67d6f6da89cdab362794bc9b003cbd58
Mauricio Lauffer <mauriciolauffer@outlook.com>
2017-12-07 22:10:02 +1100

    Demo website

--- demo/webapp/controller/App.controller.js
+++ demo/webapp/controller/App.controller.js
@@ -1,68 +1,62 @@
 sap.ui.define([
+  'sap/m/Button',
   'sap/m/PlacementType',
-  "sap/ui/core/mvc/Controller",
-  "openui5/tour/Tour",
-  "openui5/tour/TourStep"
-], function(PlacementType, Controller, Tour, TourStep) {
-  "use strict";
+  'sap/m/Text',
+  'sap/ui/core/mvc/Controller',
+  'openui5/tour/Tour',
+  'openui5/tour/TourStep'
+], function(Button, PlacementType, Text, Controller, Tour, TourStep) {
+  'use strict';
 
-  return Controller.extend("mlauffer.demo.openui5.tour.controller.App", {
+  return Controller.extend('mlauffer.demo.openui5.tour.controller.App', {
     _tour: null,
 
     onInit: function() {
       const tourSteps = [
         new TourStep({
-          content: new sap.m.Text({ text: 'Hey! It is a tour!' }),
+          content: new Text({ text: 'Hey! It is a tour!' }),
           target: this.getView().byId('panel2'),
-          title: 'Tour test step1...',
-          nextStep: function(){console.dir('next pressed from tour step1...')},
-          previousStep: function(){console.dir('previous pressed from tour step1...')}
+          title: 'Tour test step1...'
         }),
 
         new TourStep({
-          content: new sap.m.Text({ text: 'Hey! It is a tour!' }),
+          content: new Button({ text: 'Hey! It is a tour!' }),
           target: this.getView().byId('panel4'),
-          title: 'Tour test step2...',
-          nextStep: function(){console.dir('next pressed from tour step2...')},
-          previousStep: function(){console.dir('previous pressed from tour step2...')}
+          title: 'Tour test step2...'
         }),
 
         new TourStep({
-          content: new sap.m.Text({ text: 'Hey! It is a tour!' }),
+          content: new Text({ text: 'Hey! It is a tour!' }),
           target: this.getView().byId('title3'),
           title: 'Tour test step3...',
-          placement: PlacementType.Right,
-          nextStep: function(){console.dir('next pressed from tour step3...')},
-          previousStep: function(){console.dir('previous pressed from tour step3...')}
+          placement: PlacementType.Right
         }),
 
         new TourStep({
-          content: new sap.m.Text({ text: 'Hey! It is a tour!' }),
+          content: new Button({ text: 'Hey! It is a tour!' }),
           target: this.getView().byId('panel5'),
           title: 'Tour test step4...',
-          placement: PlacementType.Top,
-          nextStep: function(){console.dir('next pressed from tour step4...')},
-          previousStep: function(){console.dir('previous pressed from tour step4...')}
+          placement: PlacementType.Top
         }),
 
         new TourStep({
-          content: new sap.m.Text({ text: 'Hey! It is a tour!' }),
+          content: new Text({ text: 'Hey! It is a tour!' }),
           target: this.getView().byId('panel1'),
-          title: 'Tour test step5...',
-          nextStep: function(){console.dir('next pressed from tour step5...')},
-          previousStep: function(){console.dir('previous pressed from tour step5...')}
+          title: 'Tour test step5...'
         })
       ];
 
       this._tour = new Tour({
         steps: tourSteps,
-        start: function() {console.dir('Tour has started...')},
-        end: function(){console.dir('Tour has ended...')}
+        started: function() {console.dir('Tour has started...');},
+        completed: function(){console.dir('Tour has ended...');},
+        nextStep: function(){console.dir('next step called from tour...');},
+        previousStep: function(){console.dir('previous step called from tour...');}
       });
     },
 
     onStartTour: function() {
-      this._tour.startTour();
+      this._tour.start();
     }
   });
 });
