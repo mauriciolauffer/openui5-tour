d9178570f28edc103a5e8c1a0d5c81e326a8460d
Mauricio Lauffer <mauriciolauffer@outlook.com>
2018-09-12 22:36:55 +1000

    0.0.13

--- dist/resources/openui5/tour/library-dbg.js
+++ dist/resources/openui5/tour/library-dbg.js
@@ -7,7 +7,7 @@ sap.ui.define([], function() {
    * @namespace
    * @name openui5.tour
    * @author Mauricio Lauffer
-   * @version 0.0.12
+   * @version 0.0.13
    * @public
    */
   return sap.ui.getCore().initLibrary({
@@ -21,6 +21,6 @@ sap.ui.define([], function() {
       'openui5.tour.TourStep'
     ],
     noLibraryCSS: true,
-    version: '0.0.12'
+    version: '0.0.13'
   });
 });
