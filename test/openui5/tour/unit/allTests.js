f9bc3431928e6d4ba1b37d50957d3cc2c5f98e85
Mauricio Lauffer <443888+mauriciolauffer@users.noreply.github.com>
2023-12-05 15:54:15 +1000

    test: Memory Leak

--- test/openui5/tour/unit/allTests.js
+++ test/openui5/tour/unit/allTests.js
@@ -2,9 +2,9 @@
 
 sap.ui.require([
   'test/unit/Tour',
-  'test/unit/TourStep'
-  // 'test/unit/TourMemoryLeak',
-  // 'test/unit/TourStepMemoryLeak'
+  'test/unit/TourStep',
+  'test/unit/TourMemoryLeak',
+  'test/unit/TourStepMemoryLeak'
 ], function() {
   const node = document.createElement('div');
   node.setAttribute('id', 'content');
